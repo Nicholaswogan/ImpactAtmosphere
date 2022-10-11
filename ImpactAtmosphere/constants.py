0c3f7bab229f79cd78656244e7d033d32034f2d9
Nick Wogan <nicholaswogan@gmail.com>
2022-10-11 12:09:47 -0700

    experimenting with new model

---
+++ ImpactAtmosphere/constants.py
@@ -0,0 +1,15 @@
+import cantera as ct
+
+L_H2O = 2307.61404e7 # erg/g
+mu_H2O = 18.015 # g/mol
+R = 8.31446261815324e7 # erg/(mol*K)
+N_avo = ct.avogadro/1e3 # molecules/mol
+k_boltz = ct.boltzmann*1e7 # egs/K
+yr = 365*24*60*60.0 # s in year
+
+# heat capacity of steam gas
+cp_H2O = 1996e4 # erg/(g*K)
+T_crit_H2O = 647.096 # K, critical point of H2O
+
+# ~half machine precision tolerance for various things
+TOL = 1.0e-9
