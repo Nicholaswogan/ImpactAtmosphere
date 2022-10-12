ff3ecfc118b4dcc0e7e11e274f1e51780caae305
Nick Wogan <nicholaswogan@gmail.com>
2022-10-12 13:32:52 -0700

    new version is working well

--- ImpactAtmosphere/SteamAtm_1.py
+++ ImpactAtmosphere/SteamAtm_1.py
@@ -37,11 +37,12 @@ class SteamAtm():
             self.Ni_area = Ni_area # cm^2 Ni surface / cm^2 column of the atmosphere
 
         # Settings
-        self.rtol = 1e-12 # relative tolerance for CVODES integrator
-        self.atol = 1e-21 # absolute tolerance for CVODES integrator
-        self.dTemp = 10 # Temperature descritization (K)
+        self.rtol = 1e-10 # relative tolerance for CVODES integrator
+        self.atol = 1e-20 # absolute tolerance for CVODES integrator
+        self.dTemp = 3 # Temperature descritization (K)
+        self.T_stop = 400
         self.verbose = True
-
+        
         # Constants
         self.grav = 982 # gravity of Earth (cm/s2)
         self.area = 5.1e18 # Area of Earth (cm2)
@@ -72,6 +73,7 @@ class SteamAtm():
         N_init, P_init, X = \
         self.initial_conditions(N_H2O_ocean,N_CO2,N_N2,M_i,N_CO, N_H2, N_CH4)
         sol = self.cooling_steam_atmosphere_1(N_H2O_ocean, N_init, P_init, X)
+        sol = self.cooling_steam_atmosphere_2(sol)
         return sol.to_dict()
 
     def cooling_steam_atmosphere_1(self, N_H2O_ocean, N_init, P_init, X):
@@ -94,7 +96,7 @@ class SteamAtm():
             
             # Atmospheric properties at the beginning of the big time step
             mubar_prev, Psurf_prev, Ntot_prev, Mtot_prev, mix_prev = prep_atmosphere(Tsurf_prev, N_prev, self.gas, self.grav)
-            dtime = self.dtime_dTemp(Tsurf_prev, N_prev) # get the size of the time-step
+            dtime = self.dtime_dTemp_first(Tsurf_prev, N_prev) # get the size of the time-step
             self.gas.TPX = Tsurf_prev,Psurf_prev/10.0,mix_prev
             
             # advance chemistry forward dtime using cantera
@@ -126,13 +128,19 @@ class SteamAtm():
             mubar = self.gas.mean_molecular_weight
             Ntot = Mtot_prev/mubar
             N[:] = Ntot*self.gas.X.clip(min=0)
-            # Only save if H2O is not supersaturated
+            # break the integration if H2O is supersaturated
             if Tsurf < const.T_crit_H2O:
                 mubar, Psurf, Ntot, Mtot, mix = prep_atmosphere(Tsurf, N, self.gas, self.grav)
                 P_H2O_sat = sat_pressure_H2O(Tsurf)
                 P_H2O = Psurf*mix[self.ind_H2O]
-                if P_H2O_sat > P_H2O:
+                if P_H2O > P_H2O_sat:
+                    # Adjust N_H2O so that it is at saturation
+                    N_H2O = self.saturated_H2O_column(Tsurf, N)
+                    N[self.ind_H2O] = N_H2O
+                    # we append the adjusted column
+                    sol.append(tn, Tsurf, N)
                     break 
+            
             sol.append(tn, Tsurf, N) # save
 
             # update _prev
@@ -146,20 +154,98 @@ class SteamAtm():
         `sol` gives the initial conditions
         """
 
-        # tn = sol.time[-1]
-        # Tsurf_prev = self.T_prime
-        # Tsurf = self.T_prime
-        # N_prev = N_init.copy()
-        # N = N_init.copy()
+        # initial conditions
+        tn = sol.time[-1]
+        Tsurf_prev = sol.Tsurf[-1]
+        Tsurf = sol.Tsurf[-1]
+        N = np.empty(self.gas.n_total_species)
+        for j,name in enumerate(self.gas.species_names):
+            N[j] = sol.mix[name][-1]*sol.Ntot[-1]
+        N_prev = N.copy()
 
+        while True:
+            if self.verbose:
+                print('Atmospheric Temperature = '+"{:<8}".format('%.1f'%Tsurf_prev+' K'),end='\r')
 
+            # Atmospheric properties at the beginning of the big time step
+            mubar_prev, Psurf_prev, Ntot_prev, Mtot_prev, mix_prev = prep_atmosphere(Tsurf_prev, N_prev, self.gas, self.grav)
+            dtime = self.dtime_dTemp_second(Tsurf_prev, N_prev) # get the size of the time-step
+            self.gas.TPX = Tsurf_prev,Psurf_prev/10.0,mix_prev
 
-        return None
+            # advance chemistry forward dtime using cantera
+            r = ct.ConstPressureReactor(self.gas,energy='off')
+            if self.surface_catalyst:
+                self.surf_phase.TPX = Tsurf_prev,Psurf_prev/10.0,{'Ni':1}
+                # advance material surface for 10 seconds while keeping
+                # gas phase fixed. This makes the material properties
+                # realistic and consistent with the gas
+                self.surf_phase.advance_coverages(10,max_steps=1e7,rtol=self.rtol,atol=self.atol)
+                # volume of a 1 cm2 column of the atmosphere. I multiply scale height
+                # by 1.0 cm2
+                Hscale_prev = const.N_avo*const.k_boltz*Tsurf_prev/(mubar_prev*self.grav)
+                volume_column = Hscale_prev*1.0
+                r.volume = volume_column/1e6 # convert to m3
+                Ni_area_SI = self.Ni_area/1e2 # conver to m2
+                # Make the catalytic nickel surface part of the reactor network
+                rs = ct.ReactorSurface(kin=self.surf_phase, r=r, A=Ni_area_SI)
+            reactorNetwork = ct.ReactorNet([r])
+            reactorNetwork.rtol = self.rtol
+            reactorNetwork.atol = self.atol
+            reactorNetwork.max_steps = 1000000
+            reactorNetwork.advance(dtime)
+            self.gas.X = self.gas.X.clip(min=0) # force positive solution
 
-    def dtime_dTemp(self, T, N):  
+            # save the solution
+            tn += dtime
+            Tsurf -= self.dTemp
+            mubar = self.gas.mean_molecular_weight
+            Ntot = Mtot_prev/mubar
+            N[:] = Ntot*self.gas.X.clip(min=0)
+            # Adjust N_H2O so that it is at saturation
+            N_H2O = self.saturated_H2O_column(Tsurf, N)
+            N[self.ind_H2O] = N_H2O
+            sol.append(tn, Tsurf, N) # save
+
+            # update _prev
+            Tsurf_prev = Tsurf
+            N_prev[:] = N[:]
+
+            if Tsurf < self.T_stop:
+                break
+
+        return sol
+
+    def saturated_H2O_column(self, T, N):
+        P_H2O = sat_pressure_H2O(T)
+
+        N_tmp = N.copy()
+        N_tmp[self.ind_H2O] = 0.0
+        N_dry = np.sum(N_tmp)
+        X_dry = N_tmp/N_dry
+        self.gas.X = X_dry
+        mu_dry = self.gas.mean_molecular_weight
+
+        PP = np.empty(3)
+        PP[0] = const.mu_H2O*self.grav
+        PP[1] = N_dry*mu_dry*self.grav - P_H2O
+        PP[2] = -N_dry*P_H2O
+
+        N_H2O = np.max(np.real(np.roots(PP)))
+        return N_H2O
+
+    def dtime_dTemp_first(self, T, N):  
         mubar, Psurf, Ntot, Mtot, mix = prep_atmosphere(T, N, self.gas, self.grav)
         Fir = OLR(T)
         dT_dt = -self.grav/(const.cp_H2O*Psurf)*Fir
+        dtime = -(1/dT_dt)*self.dTemp
+        return dtime
+
+    def dtime_dTemp_second(self, T, N):  
+        mubar, Psurf, Ntot, Mtot, mix = prep_atmosphere(T, N, self.gas, self.grav)
+        Fir = OLR(T)
+        P_H2O = sat_pressure_H2O(T)
+        dT_dt = (-self.grav/(const.cp_H2O*Psurf))*Fir \
+                *(1 + const.L_H2O**2*const.mu_H2O*P_H2O/(const.cp_H2O*Psurf*const.R*T**2))**-1
         dtime = -(1/dT_dt)*self.dTemp
         return dtime
 
