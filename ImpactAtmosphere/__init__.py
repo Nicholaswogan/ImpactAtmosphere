b3b2af086e53ed3c8e58cc06531db74448868977
Nick Wogan <nicholaswogan@gmail.com>
2022-12-01 18:40:54 -0800

    nsurf_1

--- ImpactAtmosphere/__init__.py
+++ ImpactAtmosphere/__init__.py
@@ -3,7 +3,7 @@ from .coupling import output2photochem
 
 from .SteamAtm import SteamAtm
 from .SteamAtmContinuous import SteamAtmContinuous
-from .utils import mass_to_diameter, diameter_to_mass
+from .utils import mass_to_diameter
 
 import os
 zahnle_path = os.path.dirname(os.path.realpath(__file__))+'/data/'
